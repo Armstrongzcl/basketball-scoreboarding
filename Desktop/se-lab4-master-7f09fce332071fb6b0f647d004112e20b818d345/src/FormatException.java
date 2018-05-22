979814b7f3e06d3a48640be9deab6079940d3309
laf16302010003 <16302010003@fudan.edu.cn>
2018-05-22 21:58:19 +0800

    laf

--- Desktop/se-lab4-master-7f09fce332071fb6b0f647d004112e20b818d345/src/FormatException.java
+++
@@ -1,9 +0,0 @@
-public class FormatException extends Exception {
-    public FormatException() {
-        super();
-    }
-
-    public FormatException(String message) {
-        super(message);
-    }
-}
