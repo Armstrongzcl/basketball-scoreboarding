2c6145c363155359c4f50f5d2490ce8509d8b756
solarL <935018421@qq.com>
2018-06-03 22:32:24 +0800

    球队列表删除功能实现

---
+++ js/teamUtil.js
@@ -0,0 +1,39 @@
+function saveToFile(index) {
+
+	var oldTeams = "";
+	plus.io.requestFileSystem(plus.io.PRIVATE_DOC, function(fs) {
+		fs.root.getFile('datas/test.json', {
+			create: true
+		}, function(fileEntry) {
+			var reader = null;
+			fileEntry.file(function(file) {
+				reader = new plus.io.FileReader();
+				reader.onloadend = function(e) {
+					oldTeams = e.target.result;
+					if(oldTeams != "") {
+						teamsList = JSON.parse(oldTeams);
+					}
+					teamsList[index]={};
+					writeTo(fileEntry, teamsList);
+				};
+				reader.readAsText(file);
+			}, function(e) {
+				alert(e.message);
+			});
+
+		})
+	}, function(e) {
+		alert("Request file system failed: " + e.message);
+	});
+}
+
+function writeTo(fileEntry, teamsList) {
+	fileEntry.createWriter(function(writer) {
+		writer.write(JSON.stringify(teamsList));
+		writer.onerror = function(e) {
+			alert("失败" + JSON.stringify(e));
+		};
+		writer.onwrite = function(e) {}
+
+	})
+}
